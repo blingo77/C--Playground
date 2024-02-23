e21762fa7c4decb22eb7c4987eec6f61c4711f0a
Brandon Lingo <brandonlingo0@gmail.com>
2024-02-23 12:32:08 -0800

    tictactoe functions

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -2,7 +2,33 @@
 
 using namespace std;
 
+void displayBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player, char computer);
+bool checkTie(char *spaces, char player, char computer);
+
 int main(){
     
     return 0;
+}
+
+void displayBoard(char *spaces){
+
+}
+
+void playerMove(char *spaces, char player){
+
+}
+
+void computerMove(char *spaces, char computer){
+
+}
+
+bool checkWinner(char *spaces, char player, char computer){
+
+}
+
+bool checkTie(char *spaces, char player, char computer){
+
 }
