f19c53cd8c01ef7399f21e53a483399761d6f6c5
Brandon Lingo <brandonlingo0@gmail.com>
2024-02-23 13:29:29 -0800

    build displayBoard function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -9,12 +9,26 @@ bool checkWinner(char *spaces, char player, char computer);
 bool checkTie(char *spaces, char player, char computer);
 
 int main(){
+
+    char board[9] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+
+    displayBoard(board);
     
     return 0;
 }
 
 void displayBoard(char *spaces){
+    
+    for(int i = 0; i <= sizeof(spaces)/sizeof(spaces[0]); i += 3){
+        std::cout << spaces[i] << '|' << spaces[i+1] << '|' << spaces[i + 2] << '\n';
 
+        if(i <= 3 ){
+            std::cout << "------" << std::endl;
+        }
+    }
 }
 
 void playerMove(char *spaces, char player){
